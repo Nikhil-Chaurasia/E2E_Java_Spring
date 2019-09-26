afb8bdaba65ec698444d2a9670e05c65a5f7c57a
Nikhil-Chaurasia <nikhil.chaurasia52@gmail.com>
2019-09-26 18:41:04 -0400

    Adding LocationService and its implementation

--- locationweb/src/main/java/com/nik/location/repository/LocationRepository.java
+++ locationweb/src/main/java/com/nik/location/repository/LocationRepository.java
@@ -1,9 +1,9 @@
 package com.nik.location.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nik.location.model.Location;
 
-public interface LocationRepository extends CrudRepository<Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 
 }
---
+++ locationweb/src/main/java/com/nik/location/service/LocationServiceImpl.java
@@ -0,0 +1,44 @@
+package com.nik.location.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nik.location.model.Location;
+import com.nik.location.repository.LocationRepository;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+	
+	@Autowired
+	private LocationRepository locationRepository;
+
+	@Override
+	public Location saveLocation(Location location) {
+		return locationRepository.save(location);
+	}
+
+	@Override
+	public Location updateLocation(Location location) {
+		return locationRepository.save(location);
+	}
+
+	@Override
+	public void deleteLocation(Location location) {
+		locationRepository.delete(location);
+	}
+
+	@Override
+	public Location getLocationById(int id) {
+		Optional<Location> optionalLocation = locationRepository.findById(id);
+		return optionalLocation.get();
+	}
+
+	@Override
+	public List<Location> getAllLocation() {
+		return locationRepository.findAll();
+	}
+
+}
